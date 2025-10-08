3d810e86b163a804ea0d968a19f8593756fc00e7
Kumar Swaraj <123369109+kumar-swaraj@users.noreply.github.com>
2025-10-08 22:15:50 +0530

    #Format Add telegram bot project

--- 28_telegram_bot/telegram_bot.py
+++ 28_telegram_bot/telegram_bot.py
@@ -1,11 +1,12 @@
 from typing import Final
+
 from telegram import Update
 from telegram.ext import (
     Application,
     CommandHandler,
+    ContextTypes,
     MessageHandler,
     filters,
-    ContextTypes,
 )
 
 # Constants
@@ -15,15 +16,15 @@ BOT_USERNAME: Final[str] = "@name_of_bot"
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello there, Nice to meet you. Let's chat!")
+    await update.message.reply_text("Hello there, Nice to meet you. Let's chat!")  # type: ignore
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Just type something and I will respond to you!")
+    await update.message.reply_text("Just type something and I will respond to you!")  # type: ignore
 
 
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("This is a custom command.")
+    await update.message.reply_text("This is a custom command.")  # type: ignore
 
 
 # Create your own response logic
@@ -47,11 +48,11 @@ def handle_response(text: str) -> str:
 
 # Handle incoming messages
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_type: str = update.message.chat.type
-    text: str = update.message.text
+    message_type: str = update.message.chat.type  # type: ignore
+    text: str = update.message.text  # type: ignore
 
     # Log users
-    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')  # type: ignore
 
     # Handle message type
     if message_type == "group":
@@ -61,11 +62,11 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else:
             return
     else:
-        response: str = handle_response(text)
+        response = handle_response(text)
 
     # Reply
     print("Bot:", response)
-    await update.message.reply_text(response)
+    await update.message.reply_text(response)  # type: ignore
 
 
 # Error handler
@@ -86,7 +87,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
     # Errors
-    app.add_error_handler(error)
+    app.add_error_handler(error)  # type: ignore
 
     # Define a poll interval
     print("Polling...")
